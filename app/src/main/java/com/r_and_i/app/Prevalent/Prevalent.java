f317c311ffb31f8293486e29c2694b146ba944a5
Godwill Monthe <oletilemonthe@gmail.com>
2021-05-31 20:29:12 +0200

    Initial commit

---
+++ app/src/main/java/com/r_and_i/app/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.r_and_i.app.Prevalent;
+
+import com.r_and_i.app.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
